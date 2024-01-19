5cca6ee3633a73c00d1e1657234478a5d0086cce
dorota <158016@student.uwm.edu.pl>
2024-01-19 17:18:05 +0100

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'help-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
